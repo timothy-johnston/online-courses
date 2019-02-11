5bb71d748b3bf0a4cfad8a1b95750273589af422
Timothy Johnston <tedwardj11@gmail.com>
2019-02-11 14:42:32 -0500

    implemented route gaurding

--- angularAndSpringBoot/frontend/todo/src/app/app-routing.module.ts
+++ angularAndSpringBoot/frontend/todo/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGaurdService } from './service/route-gaurd.service';
 
 //welcome
 const routes: Routes = [
@@ -13,9 +14,9 @@ const routes: Routes = [
   {path: '', component: LoginComponent},  //Can specify conditions as to when these can be activated (RouteGaurdService)
   {path: 'login', component: LoginComponent},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'welcome/:name', component: WelcomeComponent},
-  {path: 'todos', component: ListTodosComponent},
-  {path: 'logout', component: LogoutComponent},
+  {path: 'welcome/:name', component: WelcomeComponent, canActivate:[RouteGaurdService]},
+  {path: 'todos', component: ListTodosComponent, canActivate:[RouteGaurdService]},
+  {path: 'logout', component: LogoutComponent, canActivate:[RouteGaurdService]},
   {path: '**', component: ErrorComponent}
 
 ];
